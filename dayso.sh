ad6984c5dca20df9d807ab4bb4b2e02174f33f8a
honhathoang <ho.nhathoang52@gmail.com>
2017-04-21 08:26:43 +0700

    commit lan 1
    tinh tong

---
+++ dayso.sh
@@ -0,0 +1,39 @@
+echo "Nhap n: "
+read n
+
+sum=0
+i=0
+
+if [[ $n -gt 0 ]]
+then
+
+	while [[ $i -lt $n ]]
+	do
+		echo "Nhap so thu $(($i+1)) "
+		read number
+		eval arr[$i]=$number
+
+		i=$(($i+1))
+	
+		#tinh tong
+		sum=$(( $sum+$number ))
+
+
+	done
+else
+	clear
+	echo "N>0"
+
+	exit 1
+fi
+
+
+echo "IN RA DAY VUA NHAP "
+i=0
+while [[ $i -lt $n ]]
+do
+	echo "arr[$i]= ${arr[$i]}"
+	i=$(($i+1))
+
+done
+echo "TONG LA $sum"
